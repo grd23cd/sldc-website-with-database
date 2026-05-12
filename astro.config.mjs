7d796f807e57b3f7ed45b8b17845d96d86c24dd7
Gerard <256761544+grd23cd@users.noreply.github.com>
2026-05-12 13:34:05 +0800

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  site: 'https://your-vercel-url.vercel.app',
-  base: '/sldc-website/',
+  site: isProd
+    ? 'https://your-vercel-url.vercel.app'
+    : 'http://localhost:4321',
+
+  base: '/', // ALWAYS use root for Vercel + dev
 
   vite: {
     plugins: [tailwindcss()]
